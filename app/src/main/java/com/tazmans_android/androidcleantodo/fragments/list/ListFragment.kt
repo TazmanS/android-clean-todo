3915870655d8fd3583e9078dd195c52650b92253
TazmanS <TazmanS123@gmail.com>
2023-03-08 15:40:09 +0200

    menu for list fragment

--- app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.tazmans_android.androidcleantodo.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.tazmans_android.androidcleantodo.R
 import com.tazmans_android.androidcleantodo.R.*
@@ -25,6 +23,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }

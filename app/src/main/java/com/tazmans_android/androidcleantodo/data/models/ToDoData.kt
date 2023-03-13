d5ada52ed9fefb7035a57d0cff9fb8e9ee6a74da
TazmanS <TazmanS123@gmail.com>
2023-03-13 16:17:29 +0200

    navigate to update with args

--- app/src/main/java/com/tazmans_android/androidcleantodo/data/models/ToDoData.kt
+++ app/src/main/java/com/tazmans_android/androidcleantodo/data/models/ToDoData.kt
@@ -1,10 +1,13 @@
 package com.tazmans_android.androidcleantodo.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.tazmans_android.androidcleantodo.data.models.Priority
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "todo_table")
+@Parcelize
 data class ToDoData (
     @PrimaryKey(autoGenerate = true)
     var id : Int,
@@ -12,4 +15,4 @@ data class ToDoData (
     var title: String,
     var priority: Priority,
     var description: String
- )
+ ): Parcelable

18dc068f75165a87cc0dbe90c3f9e0b6b4b9ee46
TazmanS <TazmanS123@gmail.com>
2023-03-14 12:43:41 +0200

    delete all

--- app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
@@ -1,7 +1,9 @@
 package com.tazmans_android.androidcleantodo.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -14,7 +16,7 @@ import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
-    private val mTodoViewModel: ToDoViewModel by viewModels()
+    private val mToDoViewModel: ToDoViewModel by viewModels()
     private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
@@ -27,7 +29,7 @@ class ListFragment : Fragment() {
         recycleView.adapter = adapter
         recycleView.layoutManager = LinearLayoutManager(requireActivity())
 
-        mTodoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
             adapter.setData(data)
         })
 
@@ -43,5 +45,24 @@ class ListFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete_all) {
+            confirmRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){ _, _ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(requireContext(), "Successfully removed everything", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure?")
+        builder.create().show()
     }
 }

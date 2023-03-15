476cbca3a50220237e8762e7ee802c9e6c2fc529
TazmanS <TazmanS123@gmail.com>
2023-03-15 21:40:30 +0200

    swipte delete item

--- app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/tazmans_android/androidcleantodo/fragments/list/ListFragment.kt
@@ -7,14 +7,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tazmans_android.androidcleantodo.R
-import com.tazmans_android.androidcleantodo.R.*
 import com.tazmans_android.androidcleantodo.data.viewModel.ToDoViewModel
 import com.tazmans_android.androidcleantodo.databinding.FragmentListBinding
 import com.tazmans_android.androidcleantodo.fragments.SharedViewModel
-import kotlinx.android.synthetic.main.fragment_list.view.*
+import com.tazmans_android.androidcleantodo.fragments.list.adapter.ListAdapter
 
 class ListFragment : Fragment() {
 
@@ -50,6 +50,20 @@ class ListFragment : Fragment() {
         val recycleView = binding.recycleView
         recycleView.adapter = adapter
         recycleView.layoutManager = LinearLayoutManager(requireActivity())
+
+        swipeToDelete(recycleView)
+    }
+
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val swipeToDeleteCallback = object : SwipeToDelete() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemToDelete = adapter.dataList[viewHolder.adapterPosition]
+                mToDoViewModel.deleteItem(itemToDelete)
+                Toast.makeText(requireContext(), "Successfully removed", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
